e779f27e14e6cf061cc2fdfa726b20fde24543b7
aukeleching <35237440+aukeleching@users.noreply.github.com>
2018-02-26 10:49:41 -1000

    Completed hackwich 5

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var alphabetArray = ["Ayye", "Bee", "Cee", "Dee", "Eee"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // set VC as delegate
+        //self.tableView.datasource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return alphabetArray.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let text = alphabetArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
 }
-

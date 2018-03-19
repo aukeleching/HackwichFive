86b7b3948d2f565ec58adab4a6a683e5867e7a84
CM Student <cmstudent@UHWO-ACM-C-02.local>
2018-03-19 10:49:54 -1000

    Completed hackwich7.

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -12,13 +12,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
-    var alphabetArray = ["Ayye", "Bee", "Cee", "Dee", "Eee"]
+    var restaurantArray = ["Kapolei Kalapawai"]
+    var restaurantImageData = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // set VC as delegate
         //self.tableView.datasource = self
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
+        
+        //part 6
+        
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        restaurantImageData = dict!.object(forKey:"restaurantImages") as! [String]
+        
+        
         
     }
 
@@ -28,15 +45,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-          return alphabetArray.count
+          return restaurantArray.count
     }
     
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        let text = alphabetArray[indexPath.row]
+        let text = restaurantArray[indexPath.row]
         cell.textLabel?.text = text
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!]
+        }
+    }
+
+    
 }
+
+
+
+
+
